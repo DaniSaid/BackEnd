39f8d7698b13cf8c8922ca1947e61ed5b0d6b92e
DaniSaid <daniela.gavilan05@gmail.com>
2022-08-03 18:34:03 -0300

    requerimiento de cantidad de caracteres agregado a user

--- src/main/java/com/portafoliodg/Model/User.java
+++ src/main/java/com/portafoliodg/Model/User.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class User {
@@ -11,7 +13,11 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
+    @NotNull
+    @Size(min = 4, max=15, message="minimo tienen que ser 4 caracteres y maximo 15")
     private String user;
+    
+    @Size(min = 8, max=15, message="minimo tienen que ser 4 caracteres y maximo 15")
     private String password;
 
     public User() {
