2f40b92496b1bcf1d8d35adf688fcb22bbbb5de8
DaniSaid <daniela.gavilan05@gmail.com>
2022-08-30 20:45:55 -0300

    ..

--- src/main/java/com/portafoliodg/PortafoliodgApplication.java
+++ src/main/java/com/portafoliodg/PortafoliodgApplication.java
@@ -3,10 +3,12 @@ package com.portafoliodg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @SpringBootApplication
 @EnableJpaRepositories
+@CrossOrigin(origins = "https://hosting-angular-e585e.web.app")
 public class PortafoliodgApplication {
 
 	public static void main(String[] args) {
